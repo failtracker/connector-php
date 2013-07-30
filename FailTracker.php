d439138bac169aa9d5bcaabedfbe72a9545e4218
Jan Rudovsky <jrudovsky@vendavo.com>
2013-07-30 16:44:08 +0200

    Initial commit

---
+++ FailTracker.php
@@ -0,0 +1,35 @@
+<?
+
+class FailTracker {
+
+	private $token = "";
+	// private $url = "http://failtracker-rest.herokuapp.com/fail/insert";
+	private $url = "http://localhost:3010/fail/insert";
+
+	function __construct($token){
+		$this->token = $token;
+	}
+
+	function send($title, $msg){
+		$data = array("title" => $title, 
+					  "content" => $msg,
+					  "token" => $this->token,
+					  "date" => time() * 1000);
+		$data_string = json_encode($data);
+		 
+		$ch = curl_init($this->url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type: application/json',
+		    'Content-Length: ' . strlen($data_string))
+		);
+		 
+		$result = curl_exec($ch);
+
+		return $result;
+	}
+}
+
+?>
